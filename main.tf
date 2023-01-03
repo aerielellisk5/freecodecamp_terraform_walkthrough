c70059d89a01798fa48c7ab1e0ef40ea71ded047
Aeriel Ellis <aerielellisk5@gmail.com>
2023-01-03 12:16:08 -0800

    finally deployed 3 ec2 instances into 3 different availiabilty zones

--- main.tf
+++ main.tf
@@ -9,6 +9,36 @@ resource "aws_vpc" "default" {
     }
 }
 
+resource "aws_subnet" "az1" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet for us-west-2a"
+  }
+}
+
+resource "aws_subnet" "az2" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet for us-west-2b"
+  }
+}
+
+resource "aws_subnet" "az3" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet for us-west-2c"
+  }
+}
+
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.default.id
 }
@@ -31,18 +61,19 @@ resource "aws_route_table" "prod-route-table" {
   }
 }
 
-resource "aws_subnet" "subnet-1" {
-    vpc_id = aws_vpc.default.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "us-west-2a"
-
-    tags = {
-        Name = "prod-subnet"
-    }
-}
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.subnet-1.id
+  subnet_id      = aws_subnet.az1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.az2.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+resource "aws_route_table_association" "c" {
+  subnet_id      = aws_subnet.az3.id
   route_table_id = aws_route_table.prod-route-table.id
 }
 
@@ -93,32 +124,25 @@ resource "aws_security_group" "allow_web" {
   }
 }
 
-resource "aws_network_interface" "web-server-nic" {
-  subnet_id       = aws_subnet.subnet-1.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.allow_web.id]
 
-}
-
-resource "aws_eip" "one" {
-  vpc                       = true
-  network_interface         = aws_network_interface.web-server-nic.id
-  associate_with_private_ip = "10.0.1.50"
-  depends_on = [
-    aws_internet_gateway.gw
-  ]
-}
+# resource "aws_eip" "one" {
+#   vpc                       = true
+#   network_interface         = aws_network_interface.web-server-nic.id
+#   associate_with_private_ip = "10.0.1.50"
+#   depends_on = [
+#     aws_internet_gateway.gw
+#   ]
+# }
 
 
-resource "aws_instance" "web-server-instance" {
+resource "aws_instance" "server-1" {
   ami = "ami-0ceecbb0f30a902a6"
   instance_type = "t2.micro"
-  availability_zone = "us-west-2a"
+  subnet_id = aws_subnet.az1.id
+security_groups = [aws_security_group.allow_web.id]
+#   availability_zone = "us-west-2a"
+#   key name
   
-  network_interface {
-    device_index = 0
-    network_interface_id = aws_network_interface.web-server-nic.id
-  }
 
   user_data = <<-EOF
                  #!/bin/bash
@@ -134,7 +158,72 @@ resource "aws_instance" "web-server-instance" {
 EOF
     
     tags = {
-        Name = "web-server"
+        Name = "server-1"
+    }
+
+}
+
+resource "aws_instance" "server-2" {
+  ami = "ami-0ceecbb0f30a902a6"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.az2.id
+security_groups = [aws_security_group.allow_web.id]
+#   availability_zone = "us-west-2a"
+#   key name
+  
+#   network_interface {
+#     device_index = 0
+#     network_interface_id = aws_network_interface.web-server-nic.id
+#   }
+
+  user_data = <<-EOF
+                 #!/bin/bash
+    yum -y update
+    yum -y install httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo '<!DOCTYPE html>' > /var/www/html/index.html
+    echo '<html lang="en">' >> /var/www/html/index.html
+    # echo '<head><title>Welcome to Green Team!</title></head>'  >> /var/www/html/index.html
+    # echo '<body style="background-color:dark green;">' >> /var/www/html/index.html
+    echo '<h1 style="color:black;">This is my Apache Webserver.</h1>' >> /var/www/html/index.html
+EOF
+    
+    tags = {
+        Name = "server-2"
+    }
+
+}
+
+resource "aws_instance" "server-3" {
+  ami = "ami-0ceecbb0f30a902a6"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.az3.id
+  security_groups = [aws_security_group.allow_web.id]
+#   security group
+#   availability_zone = "us-west-2a"
+#   key name
+  
+#   network_interface {
+#     device_index = 0
+#     network_interface_id = aws_network_interface.web-server-nic.id
+#   }
+
+  user_data = <<-EOF
+                 #!/bin/bash
+    yum -y update
+    yum -y install httpd
+    systemctl start httpd
+    systemctl enable httpd
+    echo '<!DOCTYPE html>' > /var/www/html/index.html
+    echo '<html lang="en">' >> /var/www/html/index.html
+    # echo '<head><title>Welcome to Green Team!</title></head>'  >> /var/www/html/index.html
+    # echo '<body style="background-color:dark green;">' >> /var/www/html/index.html
+    echo '<h1 style="color:black;">This is my Apache Webserver.</h1>' >> /var/www/html/index.html
+EOF
+    
+    tags = {
+        Name = "server-3"
     }
 
 }
